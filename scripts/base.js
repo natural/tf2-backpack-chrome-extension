ef94198779947ffcc3cfd5795f999d5d6b3ec9ef
Troy Melhase <troy@gci.net>
2010-02-19 17:23:37 -0900

    more options, other small tweaks.

--- scripts/base.js
+++ scripts/base.js
@@ -1,20 +1,42 @@
 var tf2ItemsUrl = "http://www.tf2items.com/";
 var steamCommunityUrl = "http://steamcommunity.com/";
+var profileIdSearchUrl = "http://www.tf2items.com/search.php?tf2items_q=";
 
+// search interface:
+// http://www.tf2items.com/search.php?tf2items_q=pnatural
+// returns: {"success":"true","profile":"76561197992805111"}
 
-function getDebug() {
-    return localStorage.debug == "true" || false;
+function lookupProfileId(v) {
+    var steamIdPattern = /\d{17}/;
+    if (v.match(steamIdPattern)) {
+	return v;
+    }
+    var searchResult;
+    var searchRequest = new XMLHttpRequest();
+    searchRequest.onreadystatechange = function() {
+	if (searchRequest.readyState == 4) {
+	    var message = eval("(" + searchRequest.responseText + ")");
+	    if (message.success == "true") {
+		searchResult = message.profile
+	    }
+	}
+    }
+    searchRequest.open("GET", profileIdSearchUrl + v, false);
+    searchRequest.send(null);
+    return searchResult;
 }
 
 
-function setDebug(v) {
-    localStorage.debug = v;
-}
+function getProfileId() { return localStorage.profileId || "" }
+function setProfileId(v) { localStorage.profileId = v }
 
 
-function getProfileId() {
-    return localStorage.profileId || "";
-}
+function getSaveLocal() { return localStorage.saveLocal == "true" || false }
+function setSaveLocal(v) { localStorage.saveLocal = v }
+
+
+function getDebug() { return localStorage.debug == "true" || false }
+function setDebug(v) { localStorage.debug = v }
 
 
 function getBackpackViewUrl() {
--- scripts/options.js
+++ scripts/options.js
@@ -1,25 +1,42 @@
-var profileIdTextbox;
-var saveButton;
-
-function save() {
-  localStorage.profileId = profileIdTextbox.value;
-  markClean();
-  chrome.extension.getBackgroundPage().backgroundInit();
-}
-
 function markDirty() {
-  saveButton.disabled = false;
+    $("#saveButton").attr("disabled", false);
 }
+
 
 function markClean() {
-  saveButton.disabled = true;
+    $("#saveButton").attr("disabled", true);
 }
+
+
+function save() {
+    var newProfileId = $("#profileId").attr("value");
+    if ( newProfileId != getProfileId()) {
+	var lookupId = lookupProfileId(newProfileId);
+	if (lookupId == null) {
+	    alert("no id found");
+	    return
+	} else {
+	    $("#profileId").attr("value", lookupId);
+	    setProfileId(lookupId);
+	}
+    }
+    setSaveLocal($("input[name=saveLocal]:checked").attr("value"));
+    setDebug($("input[name=debug]:checked").attr("value"));
+
+    markClean();
+    chrome.extension.getBackgroundPage().backgroundInit();
+}
+
 
 function optionsInit() {
-  profileIdTextbox = document.getElementById("profile-id");
-  saveButton = document.getElementById("save-button");
-  profileIdTextbox.value = localStorage.profileId || "";
-  markClean();
+    $("#profileId").attr("value", getProfileId());
+    $("#profileId").change(markDirty);
+    $("#saveButton").click(save);
+    $("#cancelButton").click(optionsInit);
+    $("input[name=debug][value=" + getDebug() + "]").attr("checked", true);
+    $("input[name=saveLocal][value=" + getSaveLocal() + "]").attr("checked", true);
+    $("input[name=debug], input[name=saveLocal]").change(markDirty);
+    markClean();
 }
 
-optionsInit();
+
